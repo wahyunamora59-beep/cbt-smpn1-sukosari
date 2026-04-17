e2ff971c9130c34159f2ceec3ddc4efef876711c
wahyunamora59-beep <wahyunamora59@gmail.com>
2026-04-17 18:45:57 +0700

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     SPREADSHEET_ID: '1zc5lh-8XWEhGYJajqooWGK3Vo89kqob1iAaIdaIiXc0',
     API_KEY: 'AIzaSyAG16CaL_CwY6Hktj6nNrxCoPjYXcJZHcE',
-    PROXY_URL: 'https://script.google.com/macros/s/AKfycbxj7o3ltynlV1h3SX3CCwWE-7PYVwVFrWEQBptdr6QxOwXQA6kpJ-v3bT07fGpRFPB7ow/exec'
+    PROXY_URL: 'https://script.google.com/macros/s/AKfycbzCGEEaroaDG5juhDVl_8hSEMan_dN0CSGzTJEJ154peJjaZh3eUv5_BFiBNzmXJilu/exec'
 };
 
 // ==================== VARIABEL GLOBAL ====================
